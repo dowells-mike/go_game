5022d0a9c358870f6d1a410b74bcffb786e84a7e
Purrfict <adedejikehinde2004@gmail.com>
2023-12-20 15:16:30 +0000

    added homepage

--- gameCode/__main__.py
+++ gameCode/__main__.py
@@ -1,7 +1,45 @@
-from PyQt6.QtWidgets import QApplication
-from go import Go
 import sys
 
-app = QApplication([])
-myGo = Go()
-sys.exit(app.exec())
+from PyQt6 import QtGui
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from go import Go
+
+class GoGameHomePage(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("GO GAME")
+        self.setGeometry(600, 600, 600, 600)
+
+        self.create_main_layout()
+
+    def create_main_layout(self):
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        layout = QVBoxLayout(central_widget)
+
+        # Header
+        header_label = QLabel("GO GAME", self)
+        header_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        header_label.setFont(QFont('Times', 80, QFont.Weight.Bold))
+        layout.addWidget(header_label, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        # Start Game Button
+        start_game_button = QPushButton("Start Game", self)
+        start_game_button.clicked.connect(self.start_new_game)
+        start_game_button.setFixedSize(200, 60)
+        layout.addWidget(start_game_button, alignment=Qt.AlignmentFlag.AlignCenter)
+
+    def start_new_game(self):
+        game_window = Go()
+        game_window.show()
+        self.close()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = GoGameHomePage()
+    window.show()
+    sys.exit(app.exec())
